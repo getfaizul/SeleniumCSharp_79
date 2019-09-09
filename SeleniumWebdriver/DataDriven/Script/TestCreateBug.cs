58dfc07947a07f4cd16ccfb6a0bde219abb2142a
Faizul Kabir <faizul.kabir@myacuity.com>
2019-09-09 00:33:17 -0400

    added the selenium csharp framework from 79 the lecture

---
+++ SeleniumWebdriver/DataDriven/Script/TestCreateBug.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeleniumWebdriver.ComponentHelper;
+using SeleniumWebdriver.PageObject;
+using SeleniumWebdriver.Settings;
+
+namespace SeleniumWebdriver.DataDriven.Script
+{
+    [TestClass]
+    public class TestCreateBug
+    {
+        private TestContext _testContext;
+
+        public TestContext TestContext
+        {
+            get { return _testContext; }
+            set { _testContext = value; }
+        }
+
+        [TestMethod]
+        [DataSource("System.Data.Odbc", @"Dsn=Excel Files;dbq=C:\downloads\ExcelData.xlsx;", "TestExcelData$", DataAccessMethod.Sequential)]
+        public void TestBug()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            HomePage hpPage = new HomePage(ObjectRepository.Driver);
+            LoginPage loginPage = hpPage.NavigateToLogin();
+            var ePage =  loginPage.Login(ObjectRepository.Config.GetUsername(), ObjectRepository.Config.GetPassword());
+            var bugPage = ePage.NavigateToDetail();
+            bugPage.SelectFromCombo(TestContext.DataRow["Severity"].ToString(), TestContext.DataRow["HardWare"].ToString(), TestContext.DataRow["OS"].ToString());
+            bugPage.TypeIn(TestContext.DataRow["Summary"].ToString(), TestContext.DataRow["Desc"].ToString());
+            bugPage.ClickSubmit();
+            bugPage.Logout();
+            Thread.Sleep(5000);
+        }
+    }
+}
